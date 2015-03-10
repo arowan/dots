0210d40cfc4159c2a48019ae5e2a297f9f54c2ea
Alex Rowan <alex@mailflow.com>
2015-03-10 09:58:35 +0000

    rename

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   var animate = new Animate(500, 250);
-  var daycare = new Habitat(10);
-  animate.start(daycare.process, daycare);
+  var habitat = new Habitat(10);
+  animate.start(habitat.process, habitat);
 });
